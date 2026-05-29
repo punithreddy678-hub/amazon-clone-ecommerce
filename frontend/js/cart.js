abd9ee890647210c5294d6622c4ac43815771c18
Mohan Reddy <punithreddy678@gmail.com>
2026-05-29 12:27:34 +0530

    Added user login session system

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -18,3 +18,16 @@ function addToCart(id){
     alert("Added To Cart");
 
 }
+function addToCart(name, price) {
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({
+        name,
+        price
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert(name + " added to cart!");
+}
