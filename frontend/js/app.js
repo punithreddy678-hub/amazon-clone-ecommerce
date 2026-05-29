4a6d514780164dbc53cbb1ae8a8c6e087f47fb22
Mohan Reddy <punithreddy678@gmail.com>
2026-05-29 15:11:59 +0530

    Added user login session system

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,8 +1,1 @@
-const productsContainer =
-document.getElementById("products");
-
-if(productsContainer){
-
-    productsContainer.innerHTML =
-    "<p>Products Loading...</p>";
-}
+console.log("ShopX App Loaded");
