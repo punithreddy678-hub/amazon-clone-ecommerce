4660d7c25e340762660a7874b281e398e73298c7
Mohan Reddy <punithreddy678@gmail.com>
2026-05-29 12:37:15 +0530

    Added user login session system

--- frontend/js/products.js
+++ frontend/js/products.js
@@ -181,3 +181,26 @@ function searchProducts() {
         }
     });
 }
+function filterCategory(category){
+
+    const cards =
+        document.querySelectorAll(".product-card");
+
+    cards.forEach(card => {
+
+        if(
+            card.dataset.category === category
+        ){
+            card.style.display = "block";
+        }
+        else{
+            card.style.display = "none";
+        }
+
+    });
+
+    window.scrollTo({
+        top:700,
+        behavior:"smooth"
+    });
+}
